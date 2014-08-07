17cba843fda1326f26d124254c73e4f2d22321ba
Anthony Humes <anthony.humes@sebomarketing.com>
2014-08-06 18:40:27 -0600

    More Updates to the Design and Added Project Files

--- index.php
+++ index.php
@@ -75,6 +75,24 @@
               </div>
               <div class="col-md-4 col-bordered">
                 <h2>Upcoming Events & Reminders</h2>
+                <div id="upcoming events" class="events-date-list">
+                  <div class="item">
+                    <div class="item-date pull-left"><span class="month">Jan</span><br /><span class="day">10</span></div>
+                    <div class="item-event pull-right">
+                      <h3><a href="#">Scheduled Surgery</a><br />Jan 10, 2014</h3>
+                      <p>A brief description of procedure and what to expect. Link to specialist providing service to get familar <a href="#">more...</a></p>
+                    </div>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="item">
+                    <div class="item-date pull-left"><span class="month">Jan</span><br /><span class="day">10</span></div>
+                    <div class="item-event pull-right">
+                      <h3><a href="#">Scheduled Surgery</a><br />Jan 1, 2014</h3>
+                      <p>A brief description of procedure and what to expect. Link to specialist providing service to get familar <a href="#">more...</a></p>
+                    </div>
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
               </div>
               <div class="col-md-4">
 
