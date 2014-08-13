3c07306f98a8299f3fc898677390479419d8427d
Anthony Humes <anthony.humes@sebomarketing.com>
2014-08-13 16:15:47 -0600

    Find a Doctor Page

---
+++ find-a-doctor.php
@@ -0,0 +1,64 @@
+<?php include('inc/page-head.php'); ?>
+<?php include('inc/page-header.php'); ?>
+<?php include('inc/page-sidebar.php'); ?>
+<main id="site-main">
+  <div class="container">
+    <div id="page-content" class="font-size-small">
+      <div class="row">
+        <div class="col-lg-10 col-lg-offset-2 col-md-9 col-md-offset-3">
+          <h1 class="page-title">Find a Doctor</h1>
+          <div class="row">
+            <div class="col-md-4 col-bordered">
+
+              <h3>Personalized Search within<br />My Arches Plan and Network</h3>
+              <ul class="text-right action-list list-unstyled colored">
+                <li><a href="/portal/home.php">Login</a></li>
+                <li><a href="/registration/">Register</a></li>
+              </ul>
+              <h3>Public Search Tools</h3>
+              <ul class="text-right list-unstyled">
+                <li><a href="#">Search A Provider Online</a></li>
+                <li><a href="#">View Provider Search Demo</a></li>
+                <li><a href="#">Provider Search Flier</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 col-bordered">
+              <h3>Mental Health &<br />Substance Abuse</h3>
+              <ul class="text-right list-unstyled">
+                <li><a href="#">Find Menthal Health or<br />Substance Abuse Provider</a></li>
+              </ul>
+              <h3>After Hours Care</h3>
+              <ul class="text-right list-unstyled">
+                <li><a href="#">Utah - Urgent Care &<br />Extended Hours Provider Directory</a></li>
+                <li><a href="#">Idaho - Urgent Care &<br />Extended Hours Provider Directory</a></li>
+              </ul>
+              <h3>Dental Providers</h3>
+              <ul class="text-right list-unstyled">
+                <li><a href="#">Find Coventry Dental</a></li>
+                <li><a href="#">Look Up Monarch Dental Provider</a></li>
+                <li><a href="#">Which Dental Netowkr Do I Belong To?</a></li>
+              </ul>
+              <h3>Public Search Tools</h3>
+              <ul class="text-right list-unstyled">
+                <li><a href="#">Peak Plus Members</a></li>
+                <li><a href="#">HMO Members</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <h3>Rural Health Care Notice</h3>
+              <p>You may be entitled to coverage for health care services from the following non- participating providers if you live or reside within 30 paved road miles of the listed providers, or if you live or reside in closer proximity to the listed providers than to Arches participating providers:</p>
+              <ul>
+                <li>Green Valley/River Clinic, Green River, Emery/Grand Counties, Utah</li>
+                <li>Halchita Clinic, San Juan County, Utah</li>
+                <li>Monument Valley Health Center,Monument Valley, Utah</li>
+              </ul>
+              <p>This list may change periodically. Please check our website or call for veriﬁcation. Please be advised that if you choose a non-participating provider you will be responsible for any charges not covered by your Arches beneﬁt plan.</p>
+              <p>If you have questions concerning your rights to see a provider on this list, you may contact Customer Service at (801) 323-6200 or 1-800-377-4161. If we do not resolve your problem, you may contact the Oﬃce of Consumer Health Assistance in the Utah Insurance Department at 1-866-350-6242.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php include('inc/page-footer.php'); ?>
