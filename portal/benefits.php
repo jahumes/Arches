3a9724cd8cf23572a8c403b677cf1a89b8b80dfb
Anthony Humes <anthony.humes@sebomarketing.com>
2014-08-13 14:08:35 -0600

    Benefits Page

---
+++ portal/benefits.php
@@ -0,0 +1,108 @@
+<?php include('../inc/page-head.php'); ?>
+<?php include('../inc/page-header.php'); ?>
+<?php include('../inc/page-sidebar.php'); ?>
+  <main id="site-main">
+    <div class="container">
+      <div id="page-content">
+        <div class="row">
+          <div class="col-lg-10 col-lg-offset-2 col-md-9 col-md-offset-3">
+            <div class="member-profile text-large">
+              <h1 class="page-title">Explanation of Benefits</h1>
+              <div class="breadcrumbs">
+                <ul class="links list-unstyled list-inline pull-left">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Members</a></li>
+                  <li><a href="#">Benefits</a></li>
+                  <li class="current">Explanation of Benefits</li>
+                </ul>
+                <div class="clearfix"></div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="box shadowed">
+                    <div class="table-responsive">
+                      <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                          <th>Claim Number / Rx Number</th>
+                          <th>Status</th>
+                          <th>Member</th>
+                          <th>Provider</th>
+                          <th>Speciality</th>
+                          <th>Start Date</th>
+                          <th>End Date</th>
+                          <th>Billed</th>
+                          <th>Paid</th>
+                          <th>Member Owes</th>
+                          <th>View EOB</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>0000-1111</td>
+                            <td>Paid</td>
+                            <td>Patient Name</td>
+                            <td>Dr. Testing</td>
+                            <td>Spinal</td>
+                            <td>12/1/2014</td>
+                            <td>12/5/2014</td>
+                            <td>$7,000.00</td>
+                            <td>$6,453.00</td>
+                            <td>$547.00</td>
+                            <td><a href="#">View</a></td>
+                          </tr>
+                          <tr>
+                            <td>0000-1111</td>
+                            <td>Paid</td>
+                            <td>Patient Name</td>
+                            <td>Dr. Testing</td>
+                            <td>Spinal</td>
+                            <td>12/1/2014</td>
+                            <td>12/5/2014</td>
+                            <td>$7,000.00</td>
+                            <td>$6,453.00</td>
+                            <td>$547.00</td>
+                            <td><a href="#">View</a></td>
+                          </tr>
+                          <tr>
+                            <td>0000-1111</td>
+                            <td>Paid</td>
+                            <td>Patient Name</td>
+                            <td>Dr. Testing</td>
+                            <td>Spinal</td>
+                            <td>12/1/2014</td>
+                            <td>12/5/2014</td>
+                            <td>$7,000.00</td>
+                            <td>$6,453.00</td>
+                            <td>$547.00</td>
+                            <td><a href="#">View</a></td>
+                          </tr>
+                          <tr>
+                            <td>0000-1111</td>
+                            <td>Paid</td>
+                            <td>Patient Name</td>
+                            <td>Dr. Testing</td>
+                            <td>Spinal</td>
+                            <td>12/1/2014</td>
+                            <td>12/5/2014</td>
+                            <td>$7,000.00</td>
+                            <td>$6,453.00</td>
+                            <td>$547.00</td>
+                            <td><a href="#">View</a></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+<?php include('../inc/page-footer.php'); ?>
